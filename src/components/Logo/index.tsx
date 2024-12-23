68173676bba30ee8901fce2ec31b41a312be8e1c
obaixii <obaidawan567@gmail.com>
2024-12-23 22:33:49 +0500

    first commit

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,11 @@
 // import ThemeToggle from "../Theme/ThemeToggle";
 import { GalleryVerticalEnd } from "lucide-react"
+import ThemeToggle from "../Theme/ThemeToggle"
 
 export default function Logo() {
   return (
     <div className="relative flex justify-between items-center p-4">
-      <div />
+      <div></div>
       <div className="font-semibold">
         <a href="/" className="flex items-center gap-2 font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
@@ -16,7 +17,9 @@ export default function Logo() {
           </div>
         </a>
       </div>
-      <div />
+      <div>
+        <ThemeToggle />
+      </div>
     </div>
   )
 }
